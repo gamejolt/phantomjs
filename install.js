c4ee93d0512c98b1aedc9297d348c9d3eee1b79e
David DeCarmine <hworld@holo-world.com>
2015-11-13 13:11:48 +0100

    Using github for the CDN url.

--- install.js
+++ install.js
@@ -20,7 +20,7 @@ var url = require('url')
 var util = require('util')
 var which = require('which')
 
-var cdnUrl = process.env.npm_config_phantomjs_cdnurl || process.env.PHANTOMJS_CDNURL ||  'https://bitbucket.org/ariya/phantomjs/downloads'
+var cdnUrl = process.env.npm_config_phantomjs_cdnurl || process.env.PHANTOMJS_CDNURL || 'https://github.com/gamejolt/phantomjs/releases/download/v' + helper.version + '-0'
 var downloadUrl = cdnUrl + '/phantomjs-' + helper.version + '-'
 
 var originalPath = process.env.PATH
